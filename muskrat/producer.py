4d64102d136d52d40aec4dea50a904e65da5aa0d
Mayev <samuell.moro@gmail.com>
2021-05-27 14:55:47 +0200

    maybe like this?

--- muskrat/producer.py
+++ muskrat/producer.py
@@ -91,7 +91,7 @@ class S3Producer( BaseProducer ):
     def s3conn(self):
         if self._s3conn is None:
             self._s3conn = boto.s3.connection.OrdinaryCallingFormat
-            self._s3conn = boto.connect_s3( self.config.s3_key, self.config.s3_secret, host=self.config.s3_host )
+            # self._s3conn = boto.connect_s3( self.config.s3_key, self.config.s3_secret, host=self.config.s3_host )
         return self._s3conn
 
     @property
